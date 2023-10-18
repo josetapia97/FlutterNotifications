4b79dbc7dace26604c291466e7fd5616faf3b17a
Jose Tapia <jose.tapia@alu.ucm.cl>
2023-10-18 16:44:29 -0300

    change_app_package_name: run

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:push_app/presentation/screens/blocs/bloc/notifications_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,11 +9,14 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Permisos'),
+        title: context
+            .select((NotificationsBloc bloc) => Text('${bloc.state.status}')),
         actions: [
-          IconButton(onPressed: (){
-            //todo: solicitar permiso
-          }, icon: const Icon(Icons.settings))
+          IconButton(
+              onPressed: () {
+                context.read<NotificationsBloc>().requestPermission();
+              },
+              icon: const Icon(Icons.settings))
         ],
       ),
       body: const _HomeView(),
